c83af0f48f4b8490c98fbb55e578616eb5639c30
turgutguvenc <turgutguvenc@icloud.com>
2023-11-01 22:33:10 +0100

    Basic Backend layers are added

---
+++ ProductSaleProject/Entities/Abstract/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Abstract
+{
+    // Any class that implemeys is a database entity class
+    public interface IEntity
+    {
+    }
+}
