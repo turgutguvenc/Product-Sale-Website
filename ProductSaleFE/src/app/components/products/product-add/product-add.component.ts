e42c34722bb44c2f620f1d42163050426ff9a085
turgutguvenc <turgutguvenc@icloud.com>
2024-01-26 00:35:42 +0100

    Product add functionality is added

--- ProductSaleFE/src/app/components/products/product-add/product-add.component.ts
+++ ProductSaleFE/src/app/components/products/product-add/product-add.component.ts
@@ -1,18 +1,35 @@
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { Router } from '@angular/router';
+import { Category } from 'src/app/models/category.model';
+import { CategoryService } from 'src/app/services/category.service';
+const SMALL_WIDTH_BREAKPOINT = 720;
 @Component({
   selector: 'app-product-add',
   templateUrl: './product-add.component.html',
   styleUrls: ['./product-add.component.css'],
 })
 export class ProductAddComponent implements OnInit {
+  isScreenSmall: boolean = false;
   productAddForm: FormGroup | undefined;
+  categories: Category[] = [];
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private breakPointObserver: BreakpointObserver,
+    private categoryService: CategoryService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.breakPointObserver
+      .observe([`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`])
+      .subscribe((state: BreakpointState) => {
+        this.isScreenSmall = state.matches;
+      });
     this.createProductAddForm();
+    this.getAllCategories();
   }
 
   createProductAddForm() {
@@ -26,5 +43,17 @@ export class ProductAddComponent implements OnInit {
 
   addProduct() {
     console.log(this.productAddForm?.value);
+  }
+  getAllCategories() {
+    this.categoryService.getCategories().subscribe((response) => {
+      if (response.success) {
+        this.categories = response.data;
+        console.log('----------------Category----------------');
+        console.log(this.categories);
+      }
+    });
+  }
+  cancelProduct() {
+    this.router.navigate(['/products']);
   }
 }
